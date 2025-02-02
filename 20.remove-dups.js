9cb1cd4f9f6091f40575cbc4b6810613caff2103
Lukas Kouril <kouril.lukas@gmail.com>
2025-02-02 11:59:27 +0700

    new sol

--- 20.remove-dups.js
+++ 20.remove-dups.js
@@ -2,14 +2,11 @@ if (head === null) return head;
     
     let current = head;
     
-    // Traverse the list until the end.
     while (current !== null && current.next !== null) {
-        // If the current node's value equals the next node's value,
-        // skip the next node.
+
         if (current.val === current.next.val) {
             current.next = current.next.next;
         } else {
-            // Move to the next node only if no deletion occurred.
             current = current.next;
         }
     }
