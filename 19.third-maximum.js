18ad53a2f2a1f383076d1b5c516a2ed088ce9fd2
Lukas Kouril <kouril.lukas@gmail.com>
2025-02-02 11:57:15 +0700

    third maximum

---
+++ 19.third-maximum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    let first = null, second = null, third = null;
+
+    for (let num of nums) {
+        if (num === first || num === second || num === third) continue;
+        
+        if (first === null || num > first) {
+            third = second;
+            second = first;
+            first = num;
+        } else if (second === null || num > second) {
+            third = second;
+            second = num;
+        } else if (third === null || num > third) {
+            third = num;
+        }
+    }
+        return third === null ? first : third;
+};
