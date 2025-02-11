e65a1c538bae9133adf304b802654939c3c4aeb9
Lukas Kouril <kouril.lukas@gmail.com>
2025-02-11 12:13:19 +0700

    insert singly linked list

--- 24.singly-linked-list.js
+++ 24.singly-linked-list.js
@@ -14,10 +14,10 @@
 // }
 
 class Node {
-    constructor(value){
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
@@ -30,16 +30,54 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = new Node(value)
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
   }
-  prepend(value){
-    const newNode = new Node(value)
-    newNode.next = this.head
-    this.head = newNode
-    this.length++
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    if (index < 0) {
+      return "invalid";
+    }
+
+    const newNode = new Node(value);
+
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return
+    }
+
+    let current = this.head;
+    let position = 0;
+
+    while (position < index - 1 && current !== null) {
+      current = current.next;
+      position += 1;
+    }
+
+    if (current === null) {
+      return "out of reach";
+    }
+
+    newNode.next = current.next;
+    current.next = newNode;
   }
 }
 
@@ -47,7 +85,5 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-myLinkedList.prepend(5);
-
-
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList());
