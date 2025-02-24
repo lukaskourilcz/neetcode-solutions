21de6e7bfd7f037ebb09794279c30898c0ec441f
Lukas Kouril <kouril.lukas@gmail.com>
2025-02-24 13:02:53 +0800

    search-insert-position

--- 27.vowel_count.js
+++ 27.vowel_count.js
@@ -1,16 +1,4 @@
 function getCount(str) {
-  //   let count = 0
-  //   let vowels = ["a", "e", "i", "o", "u"]
-  //   for(let i = 0; i < str.length; i++){
-  //     for (let j = 0; j < vowels.length; j++){
-  //       if(str[i] === vowels[j]){
-  //          count++
-
-  //          }
-  //     }
-  //   }
-  //   return count;
-
   let count = 0;
   let vowels = "aeiou";
 
