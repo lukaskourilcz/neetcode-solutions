57ee000c40633c9f768eafde508ac605156c9230
Lukas Kouril <kouril.lukas@gmail.com>
2025-03-13 13:31:24 +0700

    ugly-number fix

--- 38.ugly-number.js
+++ 38.ugly-number.js
@@ -1,20 +1,18 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isUgly = function (n) {
-  while (n % 2 === 0) {
-    n = n / 2;
-  }
-  while (n % 3 === 0) {
-    n = n / 3;
-  }
-  while (n % 5 === 0) {
-    n = n / 5;
-  }
-  if (n === 1) {
-    return true;
-  } else {
-    return false;
-  }
+var isUgly = function(n) {
+    if (n <= 0) return false;
+    
+    while(n%2 === 0){
+        n = n/2 
+    }
+    while(n%3 === 0){
+        n = n/3
+    }
+    while(n%5 === 0){
+        n = n/5
+    }
+     if(n === 1){
+        return true
+     } else {
+        return false
+     }
 };
