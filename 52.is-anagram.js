cbec0e165c628a7baaf76ef73eccd0ae2284628f
Lukas Kouril <kouril.lukas@gmail.com>
2025-04-11 18:23:36 +0900

    is-anagram

---
+++ 52.is-anagram.js
@@ -0,0 +1,8 @@
+isAnagram(s, t) {
+    if (s.length !== t.length) return false;
+  
+      const sortedS = s.split('').sort().join('');
+      const sortedT = t.split('').sort().join('');
+  
+      return sortedS === sortedT;
+      }
