d4998adeba92c64f6cb770184af2f856e952882a
Lukas Kouril <kouril.lukas@gmail.com>
2025-05-06 11:27:58 +0900

    group-anagrams

---
+++ 54.group-anagrams.js
@@ -0,0 +1,19 @@
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @return {string[][]}
+   */
+  groupAnagrams(strs) {
+    const map = {};
+
+    for (let str of strs) {
+      const key = str.split("").sort().join("");
+      if (!map[key]) {
+        map[key] = [];
+      }
+      map[key].push(str);
+    }
+
+    return Object.values(map);
+  }
+}
