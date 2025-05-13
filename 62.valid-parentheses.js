70dd420617f9ce28d3509979d3143d02f1c784d0
Lukas Kouril <kouril.lukas@gmail.com>
2025-05-13 14:04:34 +0000

    valid-parentheses.js

---
+++ 62.valid-parentheses.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+  const stack = [];
+  const map = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+  };
+
+  for (let char of s) {
+    if (char === '(' || char === '[' || char === '{') {
+      stack.push(char);
+    } else {
+      if (stack.length === 0 || stack.pop() !== map[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+    }
