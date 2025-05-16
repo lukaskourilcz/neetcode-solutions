e33816070b6aebfe42787f216947e09b33d4cca5
Lukas Kouril <kouril.lukas@gmail.com>
2025-05-16 14:28:36 +0000

    longest-consecutive-sequence

---
+++ 64.longest-consecutive-sequence.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let noDups = new Set(nums)
+        let sorted = Array.from(noDups).sort((a, b) => a - b)
+        let longest = 1;
+        let currentStreak = 1;
+
+        for (let i = 1; i < sorted.length; i++) {
+            if (sorted[i] === sorted[i - 1] + 1) {
+                currentStreak++;
+                longest = Math.max(longest, currentStreak);
+            } else {
+                currentStreak = 1;
+            }
+        }
+
+        return longest;
+    }
+}
